cac2c4826c823aedbf9cac68bd2862e3ce40103f
孟哲 <wuweierzhi1234@163.com>
2016-12-23 21:22:53 +0800

    1.0、OC调用JS传值demo

---
+++ JavaScript/JavaScript/index.js
@@ -0,0 +1,13 @@
+$(function() {
+    //alert("alert");
+    $("#blank_div").text("这是js set的值");
+    var user_info = JSON.parse(window.rrx.getUserInfo());
+    alert(user_info.name)
+    $("#rrx_content").text(user_info.name + ", " + user_info.idno + ", "+ user_info.mobile);
+});$(function() {
+    //alert("alert");
+    $("#blank_div").text("这是js set的值");
+    var user_info = JSON.parse(window.rrx.getUserInfo());
+    alert(user_info.name)
+    $("#rrx_content").text(user_info.name + ", " + user_info.idno + ", "+ user_info.mobile);
+});
